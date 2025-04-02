713dbd2696e482c159c8a8317e1ccfb2b2644b7a
dukey8 <dukeytest95@gmail.com>
2025-04-02 04:53:05 +0000

    added season cover images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,23 @@
 
+ 
  export function Home() {
+    const seasonCovers = ["dxd.webp", "new.webp", "born.jpg", "hero.jpg"]
+
     return(
         <div className="container">
             <header>
-                <img src="public/images/banner.png"/>
+                <img src="/images/banner.png"/>
             </header>
             <main>
-                
+                <div className="season-container">
+                    {seasonCovers.map((name,index) => {
+                        return (
+                            <div className="cover-container" key={index}>
+                                <img key={index} src={`/images/seasons/${name}`}  />
+                            </div>
+                        )
+                    })}
+                </div>
             </main>
             <footer>
                 <h3>Eye Catchers</h3>
